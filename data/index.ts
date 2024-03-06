1eb655c4589c96f73e5521a3023bf49f719dd743
bradpreston <bap5393@gmail.com>
2024-03-06 18:53:45 -0500

    Add bio to data

---
+++ data/index.ts
@@ -0,0 +1,2 @@
+export const bio =
+	"I am a 30 year old full stack developer from Kalamazoo, MI. I have a focus on NextJS, TypeScript, and Golang with 7 years of practice and 3 years of professional experience.";
