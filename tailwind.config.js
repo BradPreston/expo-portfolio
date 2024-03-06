86ad265cf5b0ee5ea0e76a9aaf4d117da2567a44
bradpreston <bap5393@gmail.com>
2024-03-06 18:52:50 -0500

    Add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				Poppins: ["Poppins_400Regular"]
+			},
+			colors: {
+				black: "rgb(43, 42, 52)",
+				white: "rgb(224, 225, 221)",
+				gold: "rgb(203, 167, 47)",
+				red: "rgb(233 128 120)",
+				lightred: "rgba(213, 99, 90, .25)"
 			}
 		}
 	},
