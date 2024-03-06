facac6231eaf10e223433846e3150e3ecde77c4c
bradpreston <bap5393@gmail.com>
2024-03-05 21:30:14 -0500

    Add Poppins font from Google fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,22 @@ import { Slot, useGlobalSearchParams } from "expo-router";
 import "../global.css";
 import { Head } from "@components";
 import { View } from "react-native";
+import { useFonts, Poppins_400Regular } from "@expo-google-fonts/poppins";
 
 export default function RootLayout() {
 	const { project } = useGlobalSearchParams<{ project: string }>();
+	let [fontsLoaded, fontError] = useFonts({
+		Poppins_400Regular
+	});
+
+	if (!fontsLoaded && !fontError) {
+		return null;
+	}
 
 	return (
 		<>
 			<Head title={`Brad Preston${project ? ` | ${project}` : ""}`} />
-			<View className="bg-blue-500 dark:bg-red-600 h-full">
+			<View className="bg-slate-100 dark:bg-zinc-900 h-full">
 				<Slot />
 			</View>
 		</>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
 	content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
 	presets: [require("nativewind/preset")],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				Poppins: ["Poppins_400Regular"]
+			}
+		}
 	},
 	plugins: []
 };
