6b2b235fdbb1337c6d850e9805031134a2f8aa84
bradpreston <bap5393@gmail.com>
2024-03-10 20:39:25 -0400

    Export Button and List components

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,7 @@
 export { HeadComponent as Head } from "./Head";
 export { Paragraph } from "./Paragraph";
 export { Heading } from "./Heading";
+export { List } from "./List";
+export { Button } from "./Button";
+// sections
+export { Skills } from "./sections/Skills";
