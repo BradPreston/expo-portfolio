2692eedc8c211baa618c106bc7878b4bb606f7fd
bradpreston <bap5393@gmail.com>
2024-03-11 19:38:36 -0400

    Add custom BlogQuery hook

--- query/blogQuery.tsx
+++ query/blogQuery.tsx
@@ -1,11 +1,7 @@
 import { RequestDocument, request } from "graphql-request";
 import { UseQueryResult, useQuery } from "@tanstack/react-query";
-import { graphql } from "./graphql/gql";
-import { GET_ALL_POSTS } from "./queries";
 
-const allBlogsQueryDocument = graphql(GET_ALL_POSTS);
-
-export function BlogQuery<T>(key: string, query: RequestDocument, id?: number): UseQueryResult<T, Error> {
+export function BlogQuery<T>(key: string, query: RequestDocument): UseQueryResult<T, Error> {
 	return useQuery({
 		queryKey: [key],
 		queryFn: async () => request("https://gql.hashnode.com/", query)
