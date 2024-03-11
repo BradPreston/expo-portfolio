5c0d5fef9e47346643515c7da677179fcd58ad46
bradpreston <bap5393@gmail.com>
2024-03-10 20:42:38 -0400

    Update styles and add ScrollView to _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,9 @@
 import { Slot, useGlobalSearchParams } from "expo-router";
 import "../global.css";
 import { Head } from "@components";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { useFonts, Poppins_400Regular } from "@expo-google-fonts/poppins";
+import { Main } from "@expo/html-elements";
 
 export default function RootLayout() {
 	const { project } = useGlobalSearchParams<{ project: string }>();
@@ -17,11 +18,13 @@ export default function RootLayout() {
 	return (
 		<>
 			<Head title={`Brad Preston${project ? ` | ${project}` : ""}`} />
-			<View className="bg-slate-100 dark:bg-black h-full">
-				<View className=" w-[600px] mx-auto py-20">
-					<Slot />
-				</View>
-			</View>
+			<Main className="bg-slate-100 dark:bg-black h-full">
+				<ScrollView>
+					<View className=" w-[600px] mx-auto py-20 max-w-full">
+						<Slot />
+					</View>
+				</ScrollView>
+			</Main>
 		</>
 	);
 }
