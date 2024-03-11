33237fd686ca900dd062b4c973f57013c5978a83
bradpreston <bap5393@gmail.com>
2024-03-10 20:40:24 -0400

    Add preferredStack and techIveUsed sections to data

--- data/index.ts
+++ data/index.ts
@@ -1,2 +1,6 @@
 export const bio =
 	"I am a 30 year old full stack developer from Kalamazoo, MI. I have a focus on NextJS, TypeScript, and Golang with 7 years of practice and 3 years of professional experience.";
+
+export const preferredStack = ["React", "React Native", "Expo", "TypeScript"];
+
+export const techIveUsed = ["Nativewind", "Turborepo", "Tanstack Query", "GraphQL"];
