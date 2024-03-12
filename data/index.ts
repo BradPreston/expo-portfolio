ec38aa823fefe8bc356267c0ad5ca1a4cd3fb159
bradpreston <bap5393@gmail.com>
2024-03-11 20:41:47 -0400

    Add getAge function to calculate age from today for bio. Update focus in bio

--- data/index.ts
+++ data/index.ts
@@ -1,5 +1,20 @@
-export const bio =
-	"I am a 30 year old full stack developer from Kalamazoo, MI. I have a focus on NextJS, TypeScript, and Golang with 7 years of practice and 3 years of professional experience.";
+export const bio = `I am a ${getAge(
+	"05/03/1993"
+)} year old full stack developer from Kalamazoo, MI. I have a focus on React, React Native (Expo), and Typescript with ${getAge(
+	"01/01/2017"
+)} years of practice and ${getAge("07/01/2021")} years of professional experience.`;
+
+// calculate age based on today's date
+function getAge(dateString: string) {
+	var today = new Date();
+	var birthDate = new Date(dateString);
+	var age = today.getFullYear() - birthDate.getFullYear();
+	var m = today.getMonth() - birthDate.getMonth();
+	if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+		age--;
+	}
+	return age;
+}
 
 export const preferredStack = ["React", "React Native", "Expo", "TypeScript"];
 
