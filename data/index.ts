96ec0d82fe20c99e0ca0c358896ccb6af7413de2
bradpreston <bap5393@gmail.com>
2024-03-11 20:57:15 -0400

    Add portfolio to projects list in data file

--- data/index.ts
+++ data/index.ts
@@ -1,6 +1,6 @@
 export const bio = `I am a ${getAge(
 	"05/03/1993"
-)} year old full stack developer from Kalamazoo, MI. I have a focus on React, React Native (Expo), and Typescript with ${getAge(
+)} year old full stack developer from Kalamazoo, MI. I have a focus on React, React Native (Expo), and TypeScript with ${getAge(
 	"01/01/2017"
 )} years of practice and ${getAge("07/01/2021")} years of professional experience.`;
 
@@ -19,3 +19,22 @@ function getAge(dateString: string) {
 export const preferredStack = ["React", "React Native", "Expo", "TypeScript"];
 
 export const techIveUsed = ["Nativewind", "Turborepo", "Tanstack Query", "GraphQL"];
+
+interface Project {
+	name: string;
+	url: string;
+	image: string;
+	stack: string[];
+	challenge: string;
+}
+
+export const projects: Project[] = [
+	{
+		name: "Portfolio",
+		url: "portfolio",
+		image: "bradpreston.png",
+		stack: ["React Native", "Expo", "TypeScript", "Nativewind", "TanStack Query", "GraphQL"],
+		challenge:
+			"I've built a few test projects with Expo in the past, but nothing for the web only. I wanted to grow my skill set in Expo so I made this portfolio using Expo for the web. TanStack Query is another piece of tech that I've used in the past for projects and wanted to build on. Using GraphQL to fetch data from Hashnode for my blog was very easy using TanStack Query."
+	}
+];
