7173f31e4bcc663373418580b9a1f901d58da951
bradpreston <bap5393@gmail.com>
2024-03-15 09:00:22 -0400

    Add Card components to Projects component

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -1,10 +1,18 @@
+import { Card } from "@components";
+import { projects } from "@data";
 import { Section } from "@expo/html-elements";
 import { Heading } from "components/Heading";
+import { View } from "react-native";
 
 export function Projects() {
 	return (
 		<Section className="mt-8">
 			<Heading text="My Projects" type="H2" />
+			<View className="grid grid-cols-2 gap-5">
+				{projects.map((project) => (
+					<Card project={project} key={project.name} />
+				))}
+			</View>
 		</Section>
 	);
 }
