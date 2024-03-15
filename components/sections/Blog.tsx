4c3c7d94a4517d343ced15b35908f84da3439498
bradpreston <bap5393@gmail.com>
2024-03-15 11:03:19 -0400

    Update Blog component to loop over each blog and create a card

--- components/sections/Blog.tsx
+++ components/sections/Blog.tsx
@@ -1,10 +1,23 @@
 import { Section } from "@expo/html-elements";
+import { BlogQuery, GET_ALL_POSTS } from "@query";
 import { Heading } from "components/Heading";
+import { Blog as BlogType } from "@types";
+import { Card } from "components/Card";
+import { View } from "react-native";
 
 export function Blog() {
+	const { data } = BlogQuery<BlogType>("allBlogs", GET_ALL_POSTS);
+
+	if (!data) return;
+
 	return (
 		<Section className="mt-8">
 			<Heading text="My Blog" type="H2" />
+			<View className="grid grid-cols-1 gap-5 xs:grid-cols-2">
+				{data.user.posts.edges.map(({ post }) => (
+					<Card name={post.title} url={post.url} image={post.coverImage.url} key={post.title} target="_blank" />
+				))}
+			</View>
 		</Section>
 	);
 }
