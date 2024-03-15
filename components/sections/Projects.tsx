4a2945739b5d961c9c1b482d6deda627630162a7
bradpreston <bap5393@gmail.com>
2024-03-15 09:15:38 -0400

    Update Projects component to change the grid layout on xs screen

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -8,7 +8,7 @@ export function Projects() {
 	return (
 		<Section className="mt-8">
 			<Heading text="My Projects" type="H2" />
-			<View className="grid grid-cols-1 gap-5 sm:grid-cols-2">
+			<View className="grid grid-cols-1 gap-5 xs:grid-cols-2">
 				{projects.map((project) => (
 					<Card project={project} key={project.name} />
 				))}
