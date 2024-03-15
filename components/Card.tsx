37c47689dcb72af21d7122d90d1d6310ffecab37
bradpreston <bap5393@gmail.com>
2024-03-15 11:02:21 -0400

    Update Card component to check for a local url or an external url

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,15 +1,27 @@
 import { Heading } from "@components";
-import { Project } from "@types";
 import { Link } from "expo-router";
 import { View } from "react-native";
 import { Image } from "expo-image";
+import { A } from "@expo/html-elements";
 
 interface CardProps {
-	project: Project;
+	name: string;
+	url: string;
+	image: string;
+	target?: "_blank" | "_self";
 }
 
-export function Card({ project }: CardProps) {
-	const { name, url, image } = project;
+export function Card({ name, url, image, target }: CardProps) {
+	if (url.startsWith("http")) {
+		return (
+			<View className="border-solid border-2 border-red rounded p-2">
+				<A href={url} className="flex flex-col" target={target || "_self"}>
+					<Image source={image} contentFit="cover" style={{ flex: 1, width: "100%", aspectRatio: "16/9" }} />
+					<Heading type="H3" text={name} styles="self-center text-center" />
+				</A>
+			</View>
+		);
+	}
 
 	return (
 		<View className="border-solid border-2 border-red rounded p-2">
