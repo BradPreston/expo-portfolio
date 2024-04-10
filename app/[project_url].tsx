88fea1436d298555209a35d762292b8374531066
bradpreston <bap5393@gmail.com>
2024-04-10 17:42:03 -0400

    Add error checkout to dynamic project route

--- app/[project_url].tsx
+++ app/[project_url].tsx
@@ -1,4 +1,4 @@
-import { useLocalSearchParams, useRouter } from "expo-router";
+import { useLocalSearchParams, useRootNavigationState, Redirect, useRouter } from "expo-router";
 import { View } from "react-native";
 import { projects } from "@data";
 import { Button, Heading, List, Paragraph } from "@components";
@@ -6,28 +6,20 @@ import { Image } from "expo-image";
 
 export default function Project() {
 	const { project_url } = useLocalSearchParams<{ project_url: string }>();
-
 	if (!project_url) return;
 
-	const project = projects.filter((p) => p.url === project_url)[0] || null;
+	const rootNavigationState = useRootNavigationState();
+	if (!rootNavigationState?.key) return;
 
+	const project = projects.filter((p) => p.url === project_url)[0] || null;
 	if (!project) {
-		return (
-			<View>
-				<Heading type="H1" text="No such project" />
-			</View>
-		);
+		return <Redirect href="/404" />;
 	}
 
 	const router = useRouter();
-
-	function handleGoBack() {
-		router.back();
-	}
-
 	return (
 		<View>
-			<Button text="Go Back" onPress={handleGoBack} />
+			<Button text="Go Back" onPress={() => router.back()} />
 			<Heading type="H1" text={project.name} styles="mt-5" />
 			<Image
 				source={project.image}
