698a77a6982d28363a6ffdaa43e62220fea1d8b6
bradpreston <bap5393@gmail.com>
2024-04-10 20:31:31 -0400

    Change assets directory for card data

--- data/index.ts
+++ data/index.ts
@@ -26,7 +26,7 @@ export const projects: Project[] = [
 	{
 		name: "Portfolio",
 		url: "portfolio",
-		image: "../assets/bradpreston.png",
+		image: "./assets/bradpreston.png",
 		stack: ["React Native", "Expo", "TypeScript", "Nativewind", "TanStack Query", "GraphQL"],
 		challenge:
 			"I've built a few test projects with Expo in the past, but nothing for the web only. I wanted to grow my skill set in Expo so I made this portfolio using Expo for the web. TanStack Query is another piece of tech that I've used in the past for projects and wanted to build on. Using GraphQL to fetch data from Hashnode for my blog was very easy using TanStack Query."
@@ -34,7 +34,7 @@ export const projects: Project[] = [
 	{
 		name: "One With the Riverbed",
 		url: "one-with-the-riverbed",
-		image: "../assets/onewiththeriverbed.png",
+		image: "./assets/onewiththeriverbed.png",
 		stack: ["NextJS", "TypeScript", "Tailwind", "Vercel"],
 		challenge:
 			"This is the first project I've personally built that has a real impact on a brand. I was asked to build this site to help market One With the Riverbed. We worked together to design the site in a way that best represents the band and their brand."
@@ -42,7 +42,7 @@ export const projects: Project[] = [
 	{
 		name: "Kazoo 48",
 		url: "kazoo-48",
-		image: "../assets/kazoo48.png",
+		image: "./assets/kazoo48.png",
 		stack: ["NextJS", "TypeScript", "Sass", "Stripe", "Vercel"],
 		challenge:
 			"This platform needed to securely take payments and also prevent users from signing up without paying. I opted for the Stripe checkout page so that I didn't have to handle any payments myself. Using the API, I would store if the user had a successful payment before sending the entry to the Kazoo48 team. If a user hasn't entered, they're redirected to the signup form. This solution has had zero fraudulent entries and has ran for 2 years."
